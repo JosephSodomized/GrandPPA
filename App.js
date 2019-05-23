bf923559fcf068a60385b0efcc95b4d592c39528
Michał Bałos <Michal@Michas-MBP.home>
2019-05-23 18:51:40 +0200

    Added redirect after login

--- App.js
+++ App.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
+import firebaseConfig from './firebaseConfig';
+import * as firebase from 'firebase';
+
 
 export default class App extends React.Component {
   state = {
     isLoadingComplete: false,
   };
+
+  componentWillMount(){
+    firebase.initializeApp(firebaseConfig);
+  }
 
   render() {
     if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
--- screens/AuthenticationScreen.js
+++ screens/AuthenticationScreen.js
@@ -26,30 +26,30 @@ export default class AuthenticationScreen extends React.Component {
     };
   }
 
-  async loginWithGoogle() {
+  async loginWithGoogle(navigate) {
     const clientId = clientid;
     const { type, accessToken, user } = await Google.logInAsync({ clientId });
     if (type === "success") {
-      //redirect
+      navigate("Home");
     }
   }
 
-  register(email, password) {
+  register(email, password, navigate) {
     firebase
       .auth()
       .createUserWithEmailAndPassword(email, password)
-      .then(console.log("udało się"))
+      .then(() => navigate("Home"))
       .catch(function(error) {
         alert("Coś poszło nie tak, spróbuj jeszcze raz!");
         console.log(error.message);
       });
   }
 
-  login(email, password) {
+  login(email, password, navigate) {
     firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
-      .then()
+      .then(() => navigate("Home"))
       .catch(function(error) {
         alert("Coś poszło nie tak, spróbuj jeszcze raz!");
         console.log(error.message);
@@ -62,6 +62,7 @@ export default class AuthenticationScreen extends React.Component {
   };
 
   render() {
+    const {navigate} = this.props.navigation;
     return (
       <View style={styles.container}>
         <Container>
@@ -105,7 +106,7 @@ export default class AuthenticationScreen extends React.Component {
                     rounded
                     info
                     onPress={() => {
-                      this.login(this.state.email, this.state.password);
+                      this.login(this.state.email, this.state.password, navigate);
                     }}
                     style={styles.authButton}
                   >
@@ -125,7 +126,7 @@ export default class AuthenticationScreen extends React.Component {
                     rounded
                     info
                     onPress={() => {
-                      this.register(this.state.email, this.state.password);
+                      this.register(this.state.email, this.state.password, navigate);
                     }}
                     style={styles.authButton}
                   >
@@ -165,7 +166,7 @@ export default class AuthenticationScreen extends React.Component {
                   <Button
                     rounded
                     danger
-                    onPress={this.loginWithGoogle}
+                    onPress={() => this.loginWithGoogle(navigate)}
                     style={styles.authButton}
                   >
                     <Icon
