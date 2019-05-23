8ef380c7b48538fa5649218d3226c1189dfcebaa
Dominik Malcharczyk <malcharczyk.dominik@gmail.com>
2019-05-23 16:49:18 +0200

    added styled findMe screen

--- screens/FindMeScreen.js
+++ screens/FindMeScreen.js
@@ -1,14 +1,48 @@
-import React,{Component} from 'react';
-import {Text} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, TouchableHighlight, Image } from 'react-native';
+import { Container, Header, Content, Button, Text, Left } from 'native-base';
 
-export default class FindMeScreen extends Component{
+
+export default class FindMeScreen extends Component {
     static navigationOptions = {
-        title: 'Find me',
-      };
+        headerTitle: <Image source={require('../assets/images/logo.png')} style={{width:100, height:20}}/>,
+        headerStyle: {
+        backgroundColor: 'rgba(64,64,64,1)',
+    },
+    headerTintColor: '#fff',
+    };
 
-    render(){
-        return(
-            <Text>Zgubiłem się, pomocy</Text>
+    render() {
+        return (
+            <View style={styles.container}>
+                <Container style={styles.back}>
+                    <Text style={styles.white}>Dalej coś będzie działane później</Text>
+                </Container>
+            </View>
         )
     }
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'rgba(237,199,7,1)',
+    },
+    header: {
+        backgroundColor: 'rgba(64,64,64,1)',
+        height: 90,
+        padding: 20
+    },
+    back: {
+        backgroundColor: 'rgba(64,64,64,1)'
+    },
+    logo: {
+        fontSize: 20,
+        alignSelf: 'center',
+        padding: 50
+    },
+    white:{
+        color: '#fff'
+    }
+});
