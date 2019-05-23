8c8037abd642650716aff272a03145c79ba3b915
Michał Bałos <Michal@Michas-MBP.home>
2019-05-23 19:53:37 +0200

    Added function for adding meds to firestore

--- screens/MedsScreen.js
+++ screens/MedsScreen.js
@@ -1,16 +1,50 @@
-import React from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import React from "react";
+import { ScrollView, StyleSheet, Text } from "react-native";
+import { ExpoLinksView } from "@expo/samples";
+import firebase from "firebase";
+import "@firebase/firestore";
+import { Button } from "native-base";
+import firebaseConfig from "../firebaseConfig";
 
+let db;
 export default class MedsScreen extends React.Component {
   static navigationOptions = {
-    title: 'Medicine',
+    title: "Medicine"
   };
+
+  addMedToFirestore(email, name, date, time) {
+    db = firebase.firestore();
+    let usermeds = db.collection("usermeds");
+    const usersRef = usermeds.doc(email);
+
+    usersRef.get().then(docSnapshot => {
+      if (docSnapshot.exists) {
+        usersRef.onSnapshot(doc => {
+          usersRef.update({
+            medicines: firebase.firestore.FieldValue.arrayUnion({
+              name: name,
+              date: date,
+              time: time
+            })
+          });
+        });
+      } else {
+        usersRef.set({ medicines: [] });
+      }
+    });
+  }
 
   render() {
     return (
-      <Text>Meds</Text>
+      <Button
+        rounded
+        info
+        onPress={() => {
+          this.addMedToFirestore();
+        }}
+      >
+        <Text>AddMed</Text>
+      </Button>
     );
   }
 }
-
