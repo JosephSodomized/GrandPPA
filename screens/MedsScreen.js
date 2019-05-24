688cbbfe978a6dfcafd8923171d7db6e963a36df
Michał Bałos <Michal@Michas-MBP.home>
2019-05-24 17:01:38 +0200

    List improvements

--- screens/MedsScreen.js
+++ screens/MedsScreen.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { ScrollView, StyleSheet, Text } from "react-native";
+import { ScrollView, StyleSheet, Text, View, FlatList, Modal, TouchableHighlight  } from "react-native";
 import { ExpoLinksView } from "@expo/samples";
 import firebase from "firebase";
 import "@firebase/firestore";
-import { Button } from "native-base";
+import { Button, ListItem, Content, List, Container, Left, Right, Body } from "native-base";
 import firebaseConfig from "../firebaseConfig";
 
 let db;
@@ -11,6 +11,19 @@ export default class MedsScreen extends React.Component {
   static navigationOptions = {
     title: "Medicine"
   };
+
+  constructor(...args) {
+    super(...args);
+    this.state = {
+      medicines: [],
+      email:"testdrugi",
+      modalVisible: false,
+    }
+  }
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
 
   addMedToFirestore(email, name, date, time) {
     db = firebase.firestore();
@@ -34,14 +47,17 @@ export default class MedsScreen extends React.Component {
     });
   }
 
-  getMedFromFirestore(email) {
+   getMedFromFirestore = (email) => {
     db = firebase.firestore();
     let docRef = db.collection("usermeds").doc(email);
     docRef
       .get()
-      .then(function(doc) {
+      .then(doc => {
         if (doc.exists) {
-          console.log("Document data:", doc.data());
+          let data = doc.data();
+          this.setState({
+            medicines: data.medicines,
+          })
         } else {
           console.log("No such document!");
         }
@@ -51,17 +67,65 @@ export default class MedsScreen extends React.Component {
       });
   }
 
+  componentWillMount() {
+    this.getMedFromFirestore(this.state.email);
+  }
+
   render() {
+
+    const MedicinesList = this.state.medicines.map((item, index) => (
+      <ListItem key={index} title={item.name}><Body><Text>{item.name}</Text><Text note>{item.date}</Text></Body></ListItem>
+    ));
+    
+
     return (
-      <Button
-        rounded
-        info
-        onPress={() => {
-          this.getMedFromFirestore("testdrugi");
-        }}
-      >
-        <Text>AddMed</Text>
-      </Button>
-    );
+      <View>
+
+        <Modal
+          animationType={'slide'}
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            alert('Modal has been closed.');
+          }}>
+          <View style={{marginTop: 22}}>
+            <View>
+              <Text>Hello World!</Text>
+
+              <TouchableHighlight
+                onPress={() => {
+                  this.setModalVisible(!this.state.modalVisible);
+                }}>
+                <Text>Hide Modal</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
+
+        <TouchableHighlight
+          onPress={() => {
+            this.setModalVisible(true);
+          }}>
+          <Text>Show Modal</Text>
+        </TouchableHighlight>
+ 
+
+
+        <Button
+          rounded
+          info
+          onPress={() => {
+            this.getMedFromFirestore("testdrugi");
+          }}
+          style={{position:"relative", top:"50%", left:"50%"}}
+        >
+          <Text>AddMed</Text>
+        </Button>
+ <List>
+ {MedicinesList}
+ </List>
+   
+      </View>
+    )
   }
 }
