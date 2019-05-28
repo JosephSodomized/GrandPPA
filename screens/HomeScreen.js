de3e582288f4746193ef379d058e5e713320ddcb
afternun <michal.kacper.balos@gmail.com>
2019-05-28 22:14:30 +0200

    Code formatting

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   StyleSheet,
   View,
@@ -6,82 +6,139 @@ import {
   TouchableHighlight,
   Image,
   Alert
-} from 'react-native';
+} from "react-native";
 
-import { Container, Header, Content, Button, Text, Left } from 'native-base';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faCapsules, faCogs, faGlobeAmericas, faAmbulance } from '@fortawesome/free-solid-svg-icons'
+import { Container, Header, Content, Button, Text, Left } from "native-base";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {
+  faCapsules,
+  faCogs,
+  faGlobeAmericas,
+  faAmbulance
+} from "@fortawesome/free-solid-svg-icons";
 import { Col, Row, Grid } from "react-native-easy-grid";
-import logo from '../assets/images/logo.png';
-
+import logo from "../assets/images/logo.png";
 
 export default class HomeScreen extends React.Component {
-  
   constructor(props) {
     super(props);
     this.state = {
-      email: props.navigation.state.params.email,
-    }
+      email: props.navigation.state.params.email
+    };
   }
 
   static navigationOptions = {
-    header: null,
+    header: null
   };
 
-  onPressLogo(){
-    Alert.alert('GrandppA: Projekt wykonany przez:'+`\n`+ `\n`+ ' Karolina Rutkowska'+`\n`+ 'Michał Bałos'+`\n`+ 'Michał Topolski'+`\n`+ 'Dominik Malcharczyk');
+  onPressLogo() {
+    Alert.alert(
+      "GrandppA: Projekt wykonany przez:" +
+        `\n` +
+        `\n` +
+        " Karolina Rutkowska" +
+        `\n` +
+        "Michał Bałos" +
+        `\n` +
+        "Michał Topolski" +
+        `\n` +
+        "Dominik Malcharczyk"
+    );
   }
 
-  componentWillMount(){
-    console.log(this.state.email);
-  }
+  componentWillMount() {}
 
   render() {
     const DURATION = 50000;
-    const {navigate} = this.props.navigation;
-    const {email} = this.state.email;
+    const { navigate } = this.props.navigation;
+    const { email } = this.state.email;
     return (
       <View style={styles.container}>
         <Container style={styles.back}>
-        <TouchableHighlight onPress={this.onPressLogo} underlayColor="white">
-        <Header style={styles.header}>
-          <Image  source={logo} style={{width: 305, height: 65, marginTop:10}}/>
-          </Header>
-        </TouchableHighlight>
-          
+          <TouchableHighlight onPress={this.onPressLogo} underlayColor="white">
+            <Header style={styles.header}>
+              <Image
+                source={logo}
+                style={{ width: 305, height: 65, marginTop: 10 }}
+              />
+            </Header>
+          </TouchableHighlight>
+
           <Content>
             <Grid style={styles.align}>
               <Row>
                 <Col>
-                    <Button style={styles.button} block warning 
-                    onPress={() => {navigate('Meds', {email: this.state.email});
-                    console.log(this.state.email)}}>
-                      <FontAwesomeIcon style={styles.faCapsules} icon={faCapsules} size={90} color="#fff" />
-                      <Text style={styles.TextStyle}>Medicine</Text>
-                    </Button>
+                  <Button
+                    style={styles.button}
+                    block
+                    warning
+                    onPress={() => {
+                      navigate("Meds", { email: this.state.email });
+                    }}
+                  >
+                    <FontAwesomeIcon
+                      style={styles.faCapsules}
+                      icon={faCapsules}
+                      size={90}
+                      color="#fff"
+                    />
+                    <Text style={styles.TextStyle}>Medicine</Text>
+                  </Button>
                 </Col>
                 <Col>
-                  <Button style={styles.button} block danger
-                  onPress={() => navigate('Help')}>
-                  <FontAwesomeIcon style={styles.faCapsules} icon={faAmbulance} size={90} color="#fff" />
-                  <Text style={styles.TextStyle} color="#fff">Help me!</Text></Button>
+                  <Button
+                    style={styles.button}
+                    block
+                    danger
+                    onPress={() => navigate("Help")}
+                  >
+                    <FontAwesomeIcon
+                      style={styles.faCapsules}
+                      icon={faAmbulance}
+                      size={90}
+                      color="#fff"
+                    />
+                    <Text style={styles.TextStyle} color="#fff">
+                      Help me!
+                    </Text>
+                  </Button>
                 </Col>
               </Row>
               <Row>
                 <Col>
-                  <Button style={styles.button3} block
-                  onPress={() => {navigate('FindMe', {email: this.state.email});
-                  console.log(this.state.email)}}>
-                  <FontAwesomeIcon style={styles.faCapsules} icon={faGlobeAmericas} size={90} color="#fff" />
-                  <Text style={styles.TextStyle}>Find me</Text>
+                  <Button
+                    style={styles.button3}
+                    block
+                    onPress={() => {
+                      navigate("FindMe", { email: this.state.email });
+                    }}
+                  >
+                    <FontAwesomeIcon
+                      style={styles.faCapsules}
+                      icon={faGlobeAmericas}
+                      size={90}
+                      color="#fff"
+                    />
+                    <Text style={styles.TextStyle}>Find me</Text>
                   </Button>
                 </Col>
                 <Col>
-                  <Button style={styles.button} block light
-                  onPress={() => {navigate('Settings', {email: this.state.email});
-                  console.log(this.state.email)}}>
-                  <FontAwesomeIcon style={styles.faCapsules} icon={faCogs} size={90} color="rgba(57,57,57,1)" />
-                  <Text style={styles.TextStyle2}>Settings</Text></Button>
+                  <Button
+                    style={styles.button}
+                    block
+                    light
+                    onPress={() => {
+                      navigate("Settings", { email: this.state.email });
+                    }}
+                  >
+                    <FontAwesomeIcon
+                      style={styles.faCapsules}
+                      icon={faCogs}
+                      size={90}
+                      color="rgba(57,57,57,1)"
+                    />
+                    <Text style={styles.TextStyle2}>Settings</Text>
+                  </Button>
                 </Col>
               </Row>
             </Grid>
@@ -95,51 +152,50 @@ export default class HomeScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'rgba(237,199,7,1)',
+    backgroundColor: "rgba(237,199,7,1)"
   },
-  header:{
-    backgroundColor: 'rgba(64,64,64,1)',
+  header: {
+    backgroundColor: "rgba(64,64,64,1)",
     height: 90,
     padding: 20
   },
   back: {
-    backgroundColor: 'rgba(64,64,64,1)'
+    backgroundColor: "rgba(64,64,64,1)"
   },
   logo: {
     fontSize: 20,
-    alignSelf: 'center',
+    alignSelf: "center",
     padding: 50
   },
-  TextStyle:{
-    display: 'flex',
+  TextStyle: {
+    display: "flex",
     top: 60,
     fontSize: 30,
     right: 42,
-    color:'#fff'
+    color: "#fff"
   },
-  TextStyle2:{
-    display: 'flex',
+  TextStyle2: {
+    display: "flex",
     top: 60,
     fontSize: 30,
     right: 42,
-    color:'rgba(57,57,57,1)'
+    color: "rgba(57,57,57,1)"
   },
   align: {
     margin: 10
   },
-  faCapsules:{
-    display: 'flex',
+  faCapsules: {
+    display: "flex",
     left: 70,
     top: -10
   },
   button: {
     margin: 1,
-    height: 300,
+    height: 300
   },
   button3: {
     margin: 1,
     height: 300,
-    backgroundColor: 'rgba(83,184,87,1)'
+    backgroundColor: "rgba(83,184,87,1)"
   }
 });
-
